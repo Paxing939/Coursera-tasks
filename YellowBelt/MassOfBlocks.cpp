08c5d17563472151b7b89cd5bf34f3f0196c10f5
Ilya Sachok <isachek2001@gmail.com>
2020-06-13 10:25:36 +0300

    Did Mass task of 1st week

--- YellowBelt/MassOfBlocks.cpp
+++ YellowBelt/MassOfBlocks.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <vector>
 
 int main() {
-    int N;
-    std::cin >> N;
-    std::vector<int> temperatures;
-    temperatures.reserve(N);
-    int temperature;
-    int64_t average = 0;
+    int N, R;
+    std::cin >> N >> R;
+    int W, H, D;
+    uint64_t mass = 0;
     for (size_t i = 0; i < N; ++i) {
-        std::cin >> temperature;
-        temperatures.emplace_back(temperature);
-        average += temperature;
-    }
-    average /= N;
-
-    std::vector<int> indexes_temperatures_higher_average;
-    for (size_t i = 0; i < N; ++i) {
-        if (temperatures[i] > average) {
-            indexes_temperatures_higher_average.push_back(i);
-        }
+        std::cin >> W >> H >> D;
+        mass += (uint64_t)W * (uint64_t)H * (uint64_t)D * (uint64_t)R;
     }
 
-    std::cout << indexes_temperatures_higher_average.size() << '\n';
-    for (auto index : indexes_temperatures_higher_average) {
-        std::cout << index << " ";
-    }
+    std::cout << mass;
 
     return 0;
 }
-
