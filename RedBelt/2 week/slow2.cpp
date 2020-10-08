29177cb1fd9cc75d2ee4d1b77a8d4ec8501bcf61
Ilya Sachok <isachek2001@gmail.com>
2020-10-08 16:25:17 +0300

    Did slow2 task

--- RedBelt/2 week/slow2.cpp
+++ RedBelt/2 week/slow2.cpp
@@ -4,54 +4,76 @@
 #include <algorithm>
 #include <map>
 
+#include "test_runner.h"
+
 using namespace std;
 
 class ReadingManager {
 public:
 
-  ReadingManager() : users_(100'000, -1) {}
+  ReadingManager() : users_(100'010, -1), pages_for_users_(1010), size_(0) {}
 
   void Read(int user_id, int page_count) {
+    if (page_count > 1009) {
+      throw out_of_range("Page is out of range!");
+    }
     if (users_[user_id] == -1) {
-      indexes_.push_back(user_id);
+      ++size_;
+    } else {
+      auto &vec = pages_for_users_[users_[user_id]];
+      vec.erase(find(vec.begin(), vec.end(), user_id));
     }
-    if (pages_in_users_.count(users_[user_id]) > 0) {
-      pages_in_users_[users_[user_id]].erase(
-          find(pages_in_users_[users_[user_id]].begin(),
-               pages_in_users_[users_[user_id]].end(),
-               user_id)
-      );
-      if (pages_in_users_[users_[user_id]].empty()) {
-        pages_in_users_.erase(users_[user_id]);
-      }
-    }
-    pages_in_users_[page_count].push_back(user_id);
+    pages_for_users_[page_count].push_back(user_id);
     users_[user_id] = page_count;
   }
 
   double Cheer(int user_id) const {
-    if (indexes_.empty() || users_[user_id] == -1) {
+    if (users_[user_id] == -1) {
       return 0;
     }
-    if (indexes_.size() == 1) {
+    if (size_ == 1) {
       return 1;
     }
 
-    int counter, user_pages = users_[user_id];
-    auto lower_b = pages_in_users_.lower_bound(user_pages);
-    counter = map<int, vector<int>>{pages_in_users_.begin(), lower_b}.size();
+    int counter = 0;
+    for (int i = 0; i < users_[user_id]; ++i) {
+      counter += pages_for_users_[i].size();
+    }
 
-    return counter * 1.0 / static_cast<double>(indexes_.size() - 1);
+    return counter * 1.0 / static_cast<double>(size_ - 1);
   }
 
 private:
-  map<int, vector<int>> pages_in_users_;
+  size_t size_;
+  vector<vector<int>> pages_for_users_;
   vector<int> users_;
-  vector<int> indexes_;
 };
 
+void TestSomething() {
+  ReadingManager manager;
+  manager.Read(1, 10);
+  manager.Read(2, 7);
+  manager.Read(3, 4);
+  manager.Read(4, 20);
+  ASSERT(abs(manager.Cheer(4) - 1) < 0.000001)
+  ASSERT(abs(manager.Cheer(3) - 0) < 0.000001)
+  ASSERT(abs(manager.Cheer(2) - 0.3333333) < 0.000001)
+  ASSERT(abs(manager.Cheer(1) - 0.6666666667) < 0.000001)
+
+  ReadingManager manager1;
+  ASSERT(abs(manager1.Cheer(1) - 0) < 0.000001)
+  manager1.Read(1, 10);
+  ASSERT(abs(manager1.Cheer(1) - 1) < 0.000001)
+  manager1.Read(2, 10);
+  ASSERT(abs(manager1.Cheer(1) - 0) < 0.000001)
+  manager1.Read(3, 9);
+  ASSERT(abs(manager1.Cheer(1) - 0.5) < 0.000001)
+}
 
 int main() {
+  TestRunner tr;
+  RUN_TEST(tr, TestSomething);
+
 //  freopen("input.txt", "r", stdin);
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
