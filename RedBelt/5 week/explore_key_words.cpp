84791e9ee865f6a6ee68cbd73869cc9441eb7e6e
Ilya Sachok <isachek2001@gmail.com>
2021-02-11 16:57:08 +0300

    Tried to do better algo but wasn't successfull

--- RedBelt/5 week/explore_key_words.cpp
+++ RedBelt/5 week/explore_key_words.cpp
@@ -17,17 +17,84 @@
 
 using namespace std;
 
+template<typename Iterator>
+class IteratorRange {
+public:
+  IteratorRange(Iterator begin, Iterator end)
+      : first(begin), last(end), size_(distance(first, last)) {
+  }
+
+  Iterator begin() const {
+    return first;
+  }
+
+  Iterator end() const {
+    return last;
+  }
+
+  size_t size() const {
+    return size_;
+  }
+
+private:
+  Iterator first, last;
+  size_t size_;
+};
+
+template<typename Iterator>
+class Paginator {
+private:
+  vector<IteratorRange<Iterator>> pages;
+
+public:
+  Paginator(Iterator begin, Iterator end, size_t page_size) {
+    for (size_t left = distance(begin, end); left > 0;) {
+      size_t current_page_size = min(page_size, left);
+      Iterator current_page_end = next(begin, current_page_size);
+      pages.push_back({begin, current_page_end});
+
+      left -= current_page_size;
+      begin = current_page_end;
+    }
+  }
+
+  auto begin() const {
+    return pages.begin();
+  }
+
+  auto end() const {
+    return pages.end();
+  }
+
+  size_t size() const {
+    return pages.size();
+  }
+};
+
+template<typename C>
+auto Paginate(C &c, size_t page_size) {
+  return Paginator(begin(c), end(c), page_size);
+}
+
+
 struct Stats {
   map<string, int> word_frequences;
 
-  void operator+=(const Stats &other) {
-    for (auto &[key, value] : other.word_frequences) {
-      word_frequences[key] += value;
+  void operator+=(Stats other) {
+    if (other.word_frequences.size() < word_frequences.size()) {
+      for (auto &[key, value] : other.word_frequences) {
+        word_frequences[key] += value;
+      }
+    } else {
+      for (auto &[key, value] : word_frequences) {
+        other.word_frequences[key] += value;
+      }
+      word_frequences = move(other.word_frequences);
     }
   }
 };
 
-int CountFrequency(const string &pat, const string &txt) {
+int CountFrequency(string_view pat, string_view txt) {
   string_view str_view_txt(txt);
   vector<string_view> refs;
   int previous = 0;
@@ -39,7 +106,7 @@ int CountFrequency(const string &pat, const string &txt) {
   }
   refs.push_back(str_view_txt.substr(previous, str_view_txt.size() - previous));
   int counter = 0;
-  for (auto ref : refs) {
+  for (auto &ref : refs) {
     if (ref.back() == '\n') {
       ref = ref.substr(0, ref.size() - 1);
     }
@@ -66,30 +133,39 @@ Stats ExploreLine(const set<string> &key_words, const string &line) {
 Stats ExploreKeyWordsSingleThread(const set<string> &key_words, istream &input) {
   Stats result;
   for (string line; getline(input, line);) {
-    result += ExploreLine(key_words, line);
+    result += move(ExploreLine(key_words, line));
   }
   return result;
 }
 
-Stats ExploreKeyWords(const set<string> &key_words, istream &input) {
-  vector<future<Stats>> jobs;
+Stats ParallelFunc(const set<string> &key_words, const vector<string> &lines) {
   Stats stats;
+  for (const auto &line : lines) {
+    stats += move(ExploreLine(key_words, line));
+  }
+  return stats;
+}
+
+Stats ExploreKeyWords(const set<string> &key_words, istream &input) {
+  Stats stats;
+  vector<string> lines;
   while (!input.eof()) {
     string line;
     getline(input, line);
-    jobs.push_back(async([&key_words, &line] {
-      return ExploreLine(key_words, line);
-    }));
+    if (!line.empty()) {
+      lines.push_back(move(line));
+    }
   }
 
-  vector<Stats> stats_vector;
-  stats_vector.reserve(jobs.size());
-  for (auto &job : jobs) {
-    stats_vector.push_back(job.get());
-  }
+  int batch_size = static_cast<int>(lines.size()) / 4;
+//  for (int i = 0; i < batch_size; ++i) {
+//    stats += move(async(ParallelFunc, ref(key_words), ref(lines), i * 10'000, (i + 1) * 10'000).get());
+//  }
+//  stats += move(async(ParallelFunc, ref(key_words), ref(lines), batch_size * 10'000, lines.size()).get());
 
-  for (const auto &stat : stats_vector) {
-    stats += stat;
+  for (const auto &el : Paginate(lines, batch_size)) {
+    vector<string> l(el.begin(), el.end());
+    stats += move(async(ParallelFunc, ref(key_words), ref(l)).get());
   }
 
   return stats;
